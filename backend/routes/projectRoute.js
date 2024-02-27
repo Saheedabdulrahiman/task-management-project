b092441df29cc55e6add1ff25740db295e3beec9
Saheedabdulrahiman <saheeda342@gmail.com>
2024-02-27 17:08:27 +0530

    form data extraction using build in function

--- backend/routes/projectRoute.js
+++ backend/routes/projectRoute.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const projectController = require('../controller/projectController');
 
-router.post('/addProject', projectController.addProject);
+router.post('/add-project', projectController.addProject);
 router.delete('/delete-project/:id',projectController.deleteProject)
 router.get('/list-projects',projectController.listAllProject)
 router.get('/list-projects/:id',projectController.getProjectById)
--- frontend/src/components/NewProject.jsx
+++ frontend/src/components/NewProject.jsx
@@ -1,40 +1,54 @@
-import { useRef } from "react";
+import { useRef,useState } from "react";
 import Input from "./Input";
 import Modal from "./Modal";
+import axios from 'axios';
 
-export default function NewProject({onAdd,onCancel}) {
+export default function NewProject() {
 
   const modal = useRef();
 
-  const title = useRef();
-  const description = useRef();
-  const dueDate = useRef();
 
-  function handleSave(){
-    const enteredTitle = title.current.value;
-    const enteredDescription = description.current.value;
-    const enteredDueDate = dueDate.current.value;
+ 
+
+
+ const handleSubmit = async(event) => { 
+  event.preventDefault();
+
+ const fd =  new FormData(event.target)
+const data =  Object.fromEntries(fd.entries());
 
    //validation
 
-   if(enteredTitle.trim()==="" || 
-      enteredDescription.trim()===""||
-      enteredDueDate.trim()===""){
-        //show the error modal
-        modal.current.open();
-        return;
+  //  if(title.trim()==="" || 
+  //     description.trim()===""||
+  //     date.trim()===""){
+  //       //show the error modal
+  //       modal.current.open();
+  //       return;
 
-      }
+  //     }
+
+  
 
 
-    onAdd({
-      title:enteredTitle,
-      description:enteredDescription,
-      dueDate:enteredDueDate
-    })
+
+  try {
+    const response = await axios.post('http://localhost:3000/api/add-project',data);
+    console.log("server response",response.data);
+    
+  } catch (error) {
+    console.error('Error submitting data:', error); // Log any errors
+      alert('An error occurred while submitting data. Please try again later.'); 
   }
 
+  event.target.reset();
+ }
 
+
+
+
+  
+ 
 
   return (
     <>
@@ -42,19 +56,21 @@ export default function NewProject({onAdd,onCancel}) {
         <h2 className=" text-xl font-bold text-stone-700 my-4">invalid input</h2>
         <p className=" text-stone-600 mb-4">please make sure you provide a valid value for every input</p>
       </Modal>
+      <form onSubmit={handleSubmit} >
       <div className=" w-[35rem] mt-16">
         <menu className=" flex items-center justify-end gap-4 my-4">
         
-          <li>  <button onClick={onCancel} className=" text-stone-700 hover:text-stone-950 ">cancel</button></li>
-          <li>  <button onClick={handleSave} className=" px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">save</button></li>
+          <li>  <button  className=" text-stone-700 hover:text-stone-950 ">cancel</button></li>
+          <li>  <button type="submit" className=" px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">save</button></li>
         
         </menu>
         <div>
-         <Input type ="text" ref ={title}label="title"/>
-         <Input ref={description} label="description" isTextArea={true}/>
-         <Input type = "date" ref={dueDate}  label="due date"/>
+         <Input type ="text" name="title" label="title"/>
+         <Input label="description" name="description" isTextArea={true}/>
+         <Input type = "date" name="date"  label="due date"/>
         </div>
       </div>
+      </form>
     </>
   );
 }
